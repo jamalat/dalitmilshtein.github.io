891ea8bdf0b39760e0d52bf511dc4bb37f7eaa95
dalitmilshtein <36071237+dalitmilshtein@users.noreply.github.com>
2020-06-28 14:43:37 +0300

    Update Stroop.js

--- Stroop.js
+++ Stroop.js
@@ -96,7 +96,7 @@ define(['pipAPI'], function(APIconstructor) {
      ***********************************************/
 
     API.addStimulusSets({
-        defaultStim : [{css:{color:'black', 'font-size':'100px'}}],
+        defaultStim : [{css:{color:'black', 'font-size':'50px'}}],
         Q1          : [{inherit:'defaultStim', media: 'حدد ما  نوع الشعور الذي اتاك من  الجمله عندما تخيلت الوضع الموصوف.اذا كان إيجابي اضغط   1 واذا كان حيادي اضغط 2 واذا سلبي اضغط  3'}],
         Q2          : [{inherit:'defaultStim', media: 'שאלה 2'}],    
 	    Q3          : [{inherit:'defaultStim', media: 'שאלה 3'}],

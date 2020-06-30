64fa941d91d2726f681edb0516233c14f748e67e
dalitmilshtein <36071237+dalitmilshtein@users.noreply.github.com>
2020-06-30 15:08:19 +0300

    Update v1female.js

--- experiments/arabic/v1female.js
+++ experiments/arabic/v1female.js
@@ -1,8 +1,4 @@
-define(['pipAPI'], function(APIconstructor) {
-          var API     = new APIconstructor();          
-          API.addSettings('onEnd', window.minnoJS.onEnd);
-	
-      
+ 
       
         
           
